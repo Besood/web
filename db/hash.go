a561b58066003e10b6c6e9449c0bacff9de32be4
Besood <kd1202204@st.kobedenshi.ac.jp>
2019-05-16 07:52:02 +0900

    hash

---
+++ db/hash.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func UserPassHash(pass string)(string,error){
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+    if err != nil {
+        return "", err
+    }
+    return string(hash), err
+}
+
+func UserPassMach(hash,pw string)error{
+		return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
+}
