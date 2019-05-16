42adc72eb500c4ccb5bee9667d7416943b6fe4c7
Besood <kd1202204@st.kobedenshi.ac.jp>
2019-05-16 11:46:39 +0900

    bootstrap

--- main.go
+++ main.go
@@ -14,10 +14,13 @@ func main() {
 		user.GET("/:username",routers.UserPage)
 	}
 	router.GET("/", routers.Home)
+
 	router.GET("/login", routers.Login)
 	router.POST("/login", routers.UserLogin)
+
 	router.GET("/signup", routers.Signup)
 	router.POST("/signup", routers.UserSignup)
+	
 	router.NoRoute(routers.NoRoute)
 
 	router.Run()
